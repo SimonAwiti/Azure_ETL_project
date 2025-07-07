e320d2057b2b29b7f04765a70b5e9039d8430e9b
SimonAwiti <awitisimon23@gmail.com>
2025-07-08 01:17:22 +0300

    resolved duplicate bug

--- main.tf
+++ main.tf
@@ -133,15 +133,3 @@ resource "azurerm_storage_account" "datalake" {
     ]
   }
 }
-# Create containers for raw and curated zones
-resource "azurerm_storage_container" "raw" {
-  name                  = "raw"
-  storage_account_name  = azurerm_storage_account.datalake.name
-  container_access_type = "private"
-}
-
-resource "azurerm_storage_container" "curated" {
-  name                  = "curated"
-  storage_account_name  = azurerm_storage_account.datalake.name
-  container_access_type = "private"
-}
