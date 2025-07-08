fd7854f6f854f750e54481a1e8daa6412a251fb7
SimonAwiti <awitisimon23@gmail.com>
2025-07-08 09:05:22 +0300

    temporarily flexing network rules

--- main.tf
+++ main.tf
@@ -130,7 +130,7 @@ resource "azurerm_storage_account" "datalake" {
   is_hns_enabled           = true
 
   network_rules {
-    default_action = "Deny"
+    default_action = "Allow"
     virtual_network_subnet_ids = [
       azurerm_subnet.storage.id,
       azurerm_subnet.analytics.id
