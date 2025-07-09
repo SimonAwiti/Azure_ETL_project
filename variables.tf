b50ccb67a9976ccee09419277a754b726fa4d121
SimonAwiti <awitisimon23@gmail.com>
2025-07-09 23:00:54 +0300

    changed the region

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "The Azure region where resources will be deployed."
   type        = string
-  default     = "Canada Central"
+  default     = "South Africa North"
 }
 
 variable "vnet_name" {
