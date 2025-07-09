74c9ca3e9e3ac38220be9987bdc160f7eaf0de60
SimonAwiti <awitisimon23@gmail.com>
2025-07-09 11:24:16 +0300

    changed region to UK south

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "The Azure region where resources will be deployed."
   type        = string
-  default     = "East US" # Updated region
+  default     = "UK South" # Updated region
 }
 
 variable "vnet_name" {
